70940fc8467e988f2ce7802c43ee7cdd3bc0abe3
Martin Teigen <martin@teigen.biz>
2022-05-15 18:56:13 +0200

    Add coverage

---
+++ client/__tests__/application.test.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+describe("application component", () => {
+  it("should pass", function () {});
+});
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Application } from "./application";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("app"));
+ReactDOM.render(<Application />, document.getElementById("app"));
